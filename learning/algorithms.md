5b042287cf94c7373edafd308068364caffc209c
psymbio <psymbio@gmail.com>
2021-05-17 18:38:47 +0530

    some more easy

---
+++ learning/algorithms.md
@@ -0,0 +1,1 @@
+[Moore's Majority Vote Algorithm](https://www.cs.utexas.edu/~moore/best-ideas/mjrty/)
--- solved/0169_majorityElement.cpp
+++ solved/0169_majorityElement.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int temp_int = nums[0];
+        int max_int = nums[0];
         int temp_count = 1;
         for(int i = 1; i < nums.size(); ++i) {
             if(nums[i] == max_int)
